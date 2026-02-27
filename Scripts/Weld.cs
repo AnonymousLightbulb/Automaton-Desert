868fda109cd8c22e4a96aa4a3be7a3a58a6e1eb6
AnonymousLightbulb <rbaumhaft@gmail.com>
2026-02-27 11:05:37 -0500

    added failsafes to weld and derivitive classes

--- Scripts/Weld.cs
+++ Scripts/Weld.cs
@@ -7,14 +7,22 @@ public partial class Weld : Part
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        Joiner.NodeA = Joiner.GetParent().GetPath();
-        Joiner.NodeB = Target.GetPath();
+        if (Joiner != null && Target != null)
+        {
+            Joiner.NodeA = Joiner.GetParent().GetPath();
+            Joiner.NodeB = Target.GetPath();
+        }
         base._Ready();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        if (Joiner != null && Target != null)
+        {
+            Joiner.NodeA = Joiner.GetParent().GetPath();
+            Joiner.NodeB = Target.GetPath();
+        }
         base._Process(delta);
 
     }
--- Scripts/World.cs
+++ Scripts/World.cs
@@ -4,12 +4,14 @@ public partial class World : Node2D
 {
     public static World TheWorld;
     [Export] public PackedScene CoreTemplate;
+    //[Export] public TileMapLayer Map;
 
     public RandomNumberGenerator Bob;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+
         TheWorld = this;
         Bob = new();
         base._Ready();
@@ -19,5 +21,13 @@ public partial class World : Node2D
     public override void _Process(double delta)
     {
         base._Process(delta);
+    }
+    public override void _PhysicsProcess(double delta)
+    {
+        /*if (Input.IsKeyPressed(Key.Left))
+        {
+            Map.EraseCell(Map.LocalToMap(Map.GetLocalMousePosition()));
+        }*/
+        base._PhysicsProcess(delta);
     }
 }
